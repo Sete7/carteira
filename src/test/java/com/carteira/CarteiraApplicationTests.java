7562422f239c289b405139b4b83156aba48721fd
Junio Santos <junio.santos.ps@gmail.com>
2021-05-11 22:16:52 -0300

    Commit inicial

---
+++ src/test/java/com/carteira/CarteiraApplicationTests.java
@@ -0,0 +1,15 @@
+package com.carteira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class CarteiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
